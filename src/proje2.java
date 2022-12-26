bc8a7ceabce8b85a76c411069f8f97f168bd62ca
karabariis <karabaris96@gmail.com>
2022-12-26 13:43:13 +0300

    https://app.patika.dev/karabariis

--- src/proje2.java
+++ src/proje2.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class proje2 {
+import java.util.Scanner;
+
+public class proje2 {
+    public static void main(String[] args) {
+        double KdvOrani = 0.18;
+
+        Scanner para = new Scanner(System.in);
+        System.out.print("para giriniz:");
+        Double para1 = para.nextDouble();
+
+        System.out.println("KDV oranı: "+KdvOrani+" TL");
+        double z = para1 * 18 / 100;
+        double t = para1 + z;
+        System.out.println("KDV'li fiyatı:"+t+" TL");
+
+        double x = para1 * 18 / 100;
+        double y = para1 - x;
+        System.out.println("KDV'siz fiyatı: "+para1+" TL");
+
+        System.out.println("KDV tutarı: "+x+" TL");
+    }
 }
--- src/proje3.java
+++ src/proje3.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class proje3 {
+import java.util.Scanner;
+
+public class proje3 {
+    public static void main(String[] args) {
+
+        double kdvorani, kdv=0.08, kdv1=0.18;
+
+        Scanner para = new Scanner(System.in);
+        System.out.print("para giriniz:");
+        Double para1 = para.nextDouble();
+
+        kdvorani = (para1 > 1000) ? kdv : kdv1;
+
+        double y = para1 * kdvorani;
+        System.out.println("kdv tutatı : "+y+" TL");
+
+    }
 }
