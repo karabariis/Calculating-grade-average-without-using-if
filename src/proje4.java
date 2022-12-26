11240e4587f548b189c4062db7da8e8e35ebd2ff
karabariis <karabaris96@gmail.com>
2022-12-26 18:52:31 +0300

    https://app.patika.dev/karabariis

--- src/proje4.java
+++ src/proje4.java
@@ -16,7 +16,7 @@ public class proje4 {
 
         double u = (a1+b1+c1) /2;
         Double cevre = 2*u;
-        double Alan = Math.sqrt(u* (u-a1)* (u-b1)* (u-c1));  
+        double Alan = Math.sqrt(u* (u-a1)* (u-b1)* (u-c1));
         // kök almak için math kütüpanesini çağırmamız gerekiyor.
         System.out.println("Üçgenin alanı: "+Alan);
         System.out.println("Üçgenin çevresi: "+ cevre);
